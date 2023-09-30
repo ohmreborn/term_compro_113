5ebdf69b1edc18a97b4cbda14996b228ba525c44
ohm <98101484+ohmreborn@users.noreply.github.com>
2023-09-30 16:37:54 +0700

    Update submit.py

--- term_project_4/submit.py
+++ term_project_4/submit.py
@@ -1,17 +1,6 @@
-
-# Students don't submit this code to elab
-
-# ใช้ newton methods ในการประมาณค่า square root แล้วจัดรูปนิดหน่อยเพราะคำนวณแบบนี้มันเร็วกว่าเพราะอะไรไม่รู้
-# https://study.com/learn/lesson/how-to-use-newtons-method-to-find-roots-of-equations.html
-def square_root(a,l=10**-6):
-    x = a
-    while abs(x**2 - a) >=l:
-        x = (x + a/x)*0.5
-    return x
-
-"""
-อีกอันจำไม่ได้ เพราะไม่ได้ทำ
-"""
+from elab_give import *
+# note this import using for make code works
+# if you want to submit don't import 
 
 ### Submimt your code
 

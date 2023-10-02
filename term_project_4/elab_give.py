889eef363550622c7a46722bb26d405a2175f684
ohm <98101484+ohmreborn@users.noreply.github.com>
2023-10-02 11:09:09 +0700

    Update elab_give.py

--- term_project_4/elab_give.py
+++ term_project_4/elab_give.py
@@ -2,7 +2,7 @@
 # Students don't submit this code to elab
 
 # ใช้ newton methods ในการประมาณค่า square root แล้วจัดรูปนิดหน่อยเพราะคำนวณแบบนี้มันเร็วกว่าเพราะอะไรไม่รู้
-# https://study.com/learn/lesson/how-to-use-newtons-method-to-find-roots-of-equations.html
+# https://en.wikipedia.org/wiki/Newton%27s_method
 def square_root(a,l=10**-6):
     x = a
     while abs(x**2 - a) >=l:
